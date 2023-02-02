10fecbfe7c69dec43060da1443c0908ead9fcb4e
reikan75 <reikanxvicontact@gmail.com>
2023-02-02 22:17:15 +0100

    Add Sprint Boot skeleton

---
+++ src/test/java/com/reikan/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reikan.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
